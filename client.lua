55f27134079c806197dfbf3eff86adb9f4ec4cf6
NahuEz <63286700+nahu3z@users.noreply.github.com>
2024-12-22 20:16:40 -0300

    Added 'onClientPlayerWeaponSwitch' and removed 'getPedTask'
    * Added 'onClientPlayerWeaponSwitch' to stores the current weapon and slot.
    * Removed 'getPedTask' as it was rarely used.
    * Renamed tables.
    * Reduce comments.

--- client.lua
+++ client.lua
@@ -1,40 +1,41 @@
--- Define a table containing the IDs of weapons that are allowed to use the auto-reload/recoil reset mechanic.
-local autoReloadWeapon = {
+-- Define a table containing the IDs of weapons that are allowed to use the auto-reload/recoil.
+local AUTO_RELOAD_WEAPONS = {
     [23] = true, -- Silenced
     [24] = true, -- Desert Eagle
     [25] = true  -- Shotgun
     -- Add or remove weapon IDs as needed.
 }
 
--- Event handler for when the local player fires a weapon.
--- This handler utilizes the 'quickreload' bug (by quickly switching weapon slots) to reset the reload/recoil of weapons listed in 'autoReloadWeapon'.
+local currentSlot = getPedWeaponSlot(localPlayer)
+local currentWeapon = getPedWeapon(localPlayer)
+
+-- Event handler for auto-reload
 addEventHandler("onClientPlayerWeaponFire", localPlayer, function(weaponID)
-	if autoReloadWeapon[weaponID] then
-		local currentSlot = getPedWeaponSlot(localPlayer)
+	if AUTO_RELOAD_WEAPONS[weaponID] then
 		setPedWeaponSlot(localPlayer, 0)
 		setPedWeaponSlot(localPlayer, currentSlot)
 	end
 end)
 
--- Key bind for a specific weapon (Sniper Rifle - ID 34) to reset its reload.
--- This utilizes the same 'quickreload' bug as the event handler but is triggered by pressing the 'C' key.
-bindKey("c", "down", function()
-	-- Exit if the player is not using a weapon.
-	if getPedTask(localPlayer, "secondary", 0) ~= "TASK_SIMPLE_USE_GUN" then
-		return false
-	end
+-- Update the current weapon and slot
+addEventHandler("onClientPlayerWeaponSwitch", localPlayer, function(prev, current)
+	currentSlot = current
+	currentWeapon = getPedWeapon(localPlayer, current)
+end)
 
+-- Key bind for a specific weapon (Sniper Rifle - ID 34)
+bindKey("c", "down", function()
 	-- Exit if the weapon is not 34.
-	if getPedWeapon(localPlayer) ~= 34 then
+	if currentWeapon ~= 34 then
 		return false
 	end
 
 	-- Exit if the player is not walking.
-	-- This prevents the player from stopping aiming when crouching/standing up/rolling while aiming.
+	-- This prevents stopping aiming when crouching/standing up/rolling while aiming.
 	if getPedMoveState(localPlayer) ~= "walk" then
 		return false
 	end
-	local currentSlot = getPedWeaponSlot(localPlayer)
+
 	setPedWeaponSlot(localPlayer, 0)
 	setPedWeaponSlot(localPlayer, currentSlot)
-end)
+)
--- server.lua
+++ server.lua
@@ -1,20 +1,20 @@
-local glitches = {
-    ["quickreload"] = true,
-    ["fastmove"] = true,
-    ["fastfire"] = true,
+local defaultSettings = {
     ["crouchbug"] = true,
+    ["fastfire"] = true,
+    ["fastmove"] = true,
     ["fastsprint"] = true,
+    ["quickreload"] = true,
     ["quickstand"] = true,
 }
 
 addEventHandler("onResourceStart", resourceRoot, function()
-    for glitch, isEnabled in pairs(glitches) do
+    for glitch, isEnabled in pairs(defaultSettings) do
         setGlitchEnabled(glitch, isEnabled)
     end
 end)
 
 addEventHandler("onResourceStop", resourceRoot, function()
-    for glitch, isEnabled in pairs(glitches) do
+    for glitch, isEnabled in pairs(defaultSettings) do
         setGlitchEnabled(glitch, not isEnabled)
     end
 end)
