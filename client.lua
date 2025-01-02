3977a8258270e10fb670fda149ae0ad44d429026
NahuEz <63286700+nahu3z@users.noreply.github.com>
2025-01-02 16:42:13 -0300

    Added settings in meta.xml
    Added support for 'autoreload' and 'autocbug' settings.
    
    - Implemented 'autoreload' and 'autocbug' settings in meta.xml.
    - Enabled changing these settings from the admin panel.
    - Updated server.lua to handle changes to these settings dynamically.

--- client.lua
+++ client.lua
@@ -1,3 +1,5 @@
+local isAutoReloadEnabled = false
+
 -- Define a table containing the IDs of weapons that are allowed to use the auto-reload/recoil.
 local AUTO_RELOAD_WEAPONS = {
     [23] = true, -- Silenced
@@ -11,6 +13,7 @@ local currentWeapon = getPedWeapon(localPlayer)
 
 -- Event handler for auto-reload
 addEventHandler("onClientPlayerWeaponFire", localPlayer, function(weaponID)
+
 	if AUTO_RELOAD_WEAPONS[weaponID] then
 		setPedWeaponSlot(localPlayer, 0)
 		setPedWeaponSlot(localPlayer, currentSlot)
@@ -25,6 +28,11 @@ end)
 
 -- Key bind for a specific weapon (Sniper Rifle - ID 34)
 bindKey("c", "down", function()
+	-- Exit if auto-reload is not enabled.
+	if not isAutoReloadEnabled then
+		return false
+	end
+
 	-- Exit if the weapon is not 34.
 	if currentWeapon ~= 34 then
 		return false
@@ -38,4 +46,15 @@ bindKey("c", "down", function()
 
 	setPedWeaponSlot(localPlayer, 0)
 	setPedWeaponSlot(localPlayer, currentSlot)
+end)
+
+-- Event handler for the setting change
+addEvent("onClientSettingChange", true)
+addEventHandler("onClientSettingChange", root, function(value)
+	-- Exit if the source is not the resource root.
+	if source ~= resourceRoot then
+		return false
+	end
+
+	isAutoReloadEnabled = value
 end)
--- server.lua
+++ server.lua
@@ -3,18 +3,72 @@ local defaultSettings = {
     ["fastfire"] = true,
     ["fastmove"] = true,
     ["fastsprint"] = true,
-    ["quickreload"] = true,
-    ["quickstand"] = true,
+    ["quickstand"] = true
 }
+
+local AUTO_CBUG_WEAPONS = {
+    [23] = true,
+    [24] = true
+}
+
+local function sendSetting(value)
+    if value == nil then
+        value = get("autoreload")
+    end
+    triggerClientEvent(getElementsByType("player"), "onClientSettingChange", resourceRoot, value)
+    setGlitchEnabled("quickreload", value)
+end
+
+local function toggleAutoCbug(value)
+    for weaponID, _ in pairs(AUTO_CBUG_WEAPONS) do
+        local time = value == "true" and 0 or getOriginalWeaponProperty(weaponID, "pro", "anim_breakout_time")
+        for _, skill in ipairs({ "poor", "std", "pro" }) do
+            setWeaponProperty(weaponID, skill, "anim_breakout_time", time)
+        end
+    end
+end
 
 addEventHandler("onResourceStart", resourceRoot, function()
     for glitch, isEnabled in pairs(defaultSettings) do
         setGlitchEnabled(glitch, isEnabled)
     end
+
+    setTimer(sendSetting, 200, 1)
+
+    toggleAutoCbug(get("autocbug"))
 end)
 
 addEventHandler("onResourceStop", resourceRoot, function()
-    for glitch, isEnabled in pairs(defaultSettings) do
+    setGlitchEnabled("quickreload", false)
+    for glitch, _ in pairs(defaultSettings) do
         setGlitchEnabled(glitch, false)
+    end
+end)
+
+addEventHandler("onSettingChange", root, function(setting, _, value)
+    -- Exit if the resource is not the same as this resource.
+	local resourceSetting = setting:sub(2, 5)
+    local resourceName = getResourceName(resource)
+	if resourceSetting ~= resourceName then
+		return false
+	end
+
+    value = fromJSON(value)
+    setting = setting:gsub("*"..resourceName..".", "")
+
+    if value == "true" then
+        value = true
+    elseif value == "false" then
+        value = false
+    end
+
+    if setting == "autoreload" then
+        sendSetting(value)
+        return true
+    end
+
+    if setting == "autocbug" then
+        toggleAutoCbug(value)
+        return true
     end
 end)
